170cc02756952b1ae4493ed1cf7be9340a426b38
JohnnyBoi <jroldao04@ua.pt>
2024-03-09 17:36:04 +0000

    aula 3 start

--- Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
+++ Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c1104cf9d1c11e5136b90ec56a7bdda83a44d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217ee1936ae0916b85ff2d8c1c9d2d5f2d2eee55")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
