62d71fdc68c5786e4a7cb9f7f3ec41df7884b5dd
guilherme096 <guilherme.rosa60@gmail.com>
2024-03-14 16:03:56 +0000

    Oopsie

--- Aula4/rent_a_car.sql
+++ Aula4/rent_a_car.sql
@@ -1,35 +1,72 @@
 CREATE DATABASE RENT_A_CAR;
+GO
 
 USE RENT_A_CAR;
 
-CREATE TABLE Tipo_Fornecedor (
-                                 code INT PRIMARY KEY NOT NULL,
-                                 nome VARCHAR(255)
+CREATE TABLE Tipo_Veiculo (
+    codigo INT PRIMARY KEY,
+    designacao VARCHAR(255),
+    arcondicionado BIT
 );
+GO
 
-CREATE TABLE Fornecedor (
-                            nome VARCHAR(50),
-                            NIF INT PRIMARY KEY NOT NULL CHECK (NIF > 0),
-                            addr VARCHAR(50) NOT NULL,
-                            fax VARCHAR(50)NOT NULL,
-                            cond_pagamento INT NOT NULL CHECK(cond_pagamento > 0),
-                            tipo_fornecedor INT NOT NULL ,
-                            FOREIGN KEY (tipo_fornecedor) REFERENCES Tipo_Fornecedor(code)
+CREATE TABLE Veiculo (
+    matricula VARCHAR(255) PRIMARY KEY,
+    marca VARCHAR(255),
+    ano INT,
+    tipo INT FOREIGN KEY REFERENCES Tipo_Veiculo(codigo)
 );
+GO
 
-CREATE TABLE Produto (
-                         code INT PRIMARY KEY NOT NULL,
-                         nome VARCHAR(50) NOT NULL,
-                         iva DECIMAL(5, 2) NOT NULL,
-                         stock INT NOT NULL CHECK (stock >= 0),
+CREATE TABLE Cliente (
+    NIF INT PRIMARY KEY,
+    num_carta VARCHAR(255),
+    endereco VARCHAR(255),
+    nome VARCHAR(255)
 );
+CREATE TABLE CLIENTE (
+	NIF INT NOT NULL,
+	nome VARCHAR(30) NOT NULL,
+	endereco VARCHAR(30) NOT NULL,
+	CHECK (NIF > 0 AND NIF < 999999),
+	PRIMARY KEY (NIF)
+);
+GO
 
-CREATE TABLE Encomenda (
-                           data DATE NOT NULL ,
-                           num_unidade INT NOT NULL ,
-                           product_code INT NOT NULL,
-                           supplier_code INT NOT NULL ,
-                           PRIMARY KEY (product_code),
-                           FOREIGN KEY (product_code) REFERENCES Produto(code),
-                           FOREIGN KEY (supplier_code) REFERENCES Fornecedor(NIF)
+CREATE TABLE Balcao (
+    numero INT PRIMARY KEY,
+    nome VARCHAR(255),
+    endereco VARCHAR(255)
 );
+GO
+
+CREATE TABLE Aluguer (
+    numero INT PRIMARY KEY,
+    duracao INT,
+    data DATE,
+    client VARCHAR(255) FOREIGN KEY REFERENCES Cliente(NIF),
+    balcao INT FOREIGN KEY REFERENCES Balcao(numero),
+    veiculo VARCHAR(255) FOREIGN KEY REFERENCES Veiculo(matricula)
+);
+GO
+
+CREATE TABLE Ligeiro (
+    codigo INT PRIMARY KEY FOREIGN KEY REFERENCES Tipo_Veiculo(codigo),
+    num_lugares INT,
+    portas INT,
+    combustivel VARCHAR(255)
+);
+GO
+
+CREATE TABLE Pesado (
+    codigo INT PRIMARY KEY FOREIGN KEY REFERENCES Tipo_Veiculo(codigo),
+    peso DECIMAL(10,2),
+    passageiros INT
+);
+GO
+
+CREATE TABLE Similaridade (
+    Tipo1 INT FOREIGN KEY REFERENCES Tipo_Veiculo(codigo),
+    Tipo2 INT FOREIGN KEY REFERENCES Tipo_Veiculo(codigo)
+);
+GO
