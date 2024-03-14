e79b3b6aa5f237186311c9b58d53cec496f3e83c
guilherme096 <guilherme.rosa60@gmail.com>
2024-03-14 15:23:05 +0000

    rent a car

---
+++ Aula4/rent_a_car.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE RENT_A_CAR;
+
+USE RENT_A_CAR;
+
+CREATE TABLE Tipo_Fornecedor (
+                                 code INT PRIMARY KEY NOT NULL,
+                                 nome VARCHAR(255)
+);
+
+CREATE TABLE Fornecedor (
+                            nome VARCHAR(50),
+                            NIF INT PRIMARY KEY NOT NULL CHECK (NIF > 0),
+                            addr VARCHAR(50) NOT NULL,
+                            fax VARCHAR(50)NOT NULL,
+                            cond_pagamento INT NOT NULL CHECK(cond_pagamento > 0),
+                            tipo_fornecedor INT NOT NULL ,
+                            FOREIGN KEY (tipo_fornecedor) REFERENCES Tipo_Fornecedor(code)
+);
+
+CREATE TABLE Produto (
+                         code INT PRIMARY KEY NOT NULL,
+                         nome VARCHAR(50) NOT NULL,
+                         iva DECIMAL(5, 2) NOT NULL,
+                         stock INT NOT NULL CHECK (stock >= 0),
+);
+
+CREATE TABLE Encomenda (
+                           data DATE NOT NULL ,
+                           num_unidade INT NOT NULL ,
+                           product_code INT NOT NULL,
+                           supplier_code INT NOT NULL ,
+                           PRIMARY KEY (product_code),
+                           FOREIGN KEY (product_code) REFERENCES Produto(code),
+                           FOREIGN KEY (supplier_code) REFERENCES Fornecedor(NIF)
+);
