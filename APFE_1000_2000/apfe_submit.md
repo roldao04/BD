638fdfd59faa3a97069a98d50c0fe8cea84e1374
JohnnyBoi <jroldao04@ua.pt>
2024-03-21 15:43:12 +0000

    start entrega intremedia projeto final

---
+++ APFE_1000_2000/apfe_submit.md
@@ -0,0 +1,21 @@
+# BD: Trabalho Prático APFE
+
+**Grupo**: PXGY
+- João Roldão, MEC: 113920
+- Guilherme Rosa, MEC: 113968
+
+## Introdução / Introduction
+ 
+Base de dados focada em atributos detalhados de jogadores de futebol. É possível realizar todas as operações CRUD em jogadores e atributos. O objetivo é poder comparar jogadores em vários aspetos, tal como analisar diferentes equipas com base nos jogadores (calcular odds de matchups por exemplo).
+
+
+## ​Análise de Requisitos / Requirements
+
+
+## DER
+
+![DER Diagram!](der.jpg "AnImage")
+
+## ER
+
+![ER Diagram!](er.jpg "AnImage")
--- Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
+++ Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb7a4b2c1a4e179bd89fc6416c5e657a1ffba46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3841be6f1e218cd249d01cc2778144eda9d0c5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
