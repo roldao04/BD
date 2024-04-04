197982c7f7538d5a5f0ee69a92f006f8913cc086
JohnnyBoi <jroldao04@ua.pt>
2024-04-04 09:54:47 +0100

    dá fix gui

--- Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
+++ Aula1/MyWinFormsApp/obj/Debug/net8.0-windows/MyWinFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0039e8218ff4a0639e0c75abc030b3c4f08cce3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d86abe19f5e9c9051cf4dafcdfe10a23a9d680")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
